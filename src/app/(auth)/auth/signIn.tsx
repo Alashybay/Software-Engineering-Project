5a9f68672404ab9c9241645838ae289ac6148b5f
Ishen Imerov <macbook_pro@MBP-MBP.lan>
2024-05-27 16:56:04 +0200

    auth ui wip

--- src/app/(auth)/auth/signIn.tsx
+++ src/app/(auth)/auth/signIn.tsx
@@ -1,11 +1,8 @@
 "use client";
 
+import { Auth } from "@/src/components/auth";
 import { Text } from "@mantine/core";
 
 export default function Page() {
-    return (
-        <Text>
-            Here is the sign in page.
-        </Text>
-    );
+  return <Auth />;
 }
