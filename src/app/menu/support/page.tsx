dbaf5e4fd82de58da5646e36ce81a9b9f225c454
Alashybay <alikalikhan01@gmail.com>
2024-05-28 18:40:17 +0200

    faq and support page

--- src/app/menu/support/page.tsx
+++ src/app/menu/support/page.tsx
@@ -1,12 +1,94 @@
 "use client";
 
+import FaqSimple from "@/src/components/FaqSimple";
 import { Layout } from "@/src/components/Layout";
-import { Text } from "@mantine/core";
+import {
+  TextInput,
+  Textarea,
+  SimpleGrid,
+  Group,
+  Title,
+  Button,
+  Stack,
+  Divider,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 export default function Page() {
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    },
+    validate: {
+      name: (value) => value.trim().length < 2,
+      email: (value) => !/^\S+@\S+$/.test(value),
+      subject: (value) => value.trim().length === 0,
+    },
+  });
+
   return (
     <Layout>
-      <Text>Support!</Text>
+      <Stack gap="md">
+        <FaqSimple />
+        <Divider variant="dashed"/>
+        <form onSubmit={form.onSubmit(() => {})}>
+          <Title
+            order={2}
+            size="h1"
+            style={{ fontFamily: "Greycliff CF, var(--mantine-font-family)" }}
+            fw={900}
+            ta="center"
+          >
+            Have an extra question? Get in touch
+          </Title>
+
+          <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
+            <TextInput
+              label="Name"
+              placeholder="Your name"
+              name="name"
+              variant="filled"
+              {...form.getInputProps("name")}
+            />
+            <TextInput
+              label="Email"
+              placeholder="Your email"
+              name="email"
+              variant="filled"
+              {...form.getInputProps("email")}
+            />
+          </SimpleGrid>
+
+          <TextInput
+            label="Subject"
+            placeholder="Subject"
+            mt="md"
+            name="subject"
+            variant="filled"
+            {...form.getInputProps("subject")}
+          />
+          <Textarea
+            mt="md"
+            label="Message"
+            placeholder="Your message"
+            maxRows={10}
+            minRows={5}
+            autosize
+            name="message"
+            variant="filled"
+            {...form.getInputProps("message")}
+          />
+
+          <Group justify="center" mt="xl">
+            <Button type="submit" size="md">
+              Send message
+            </Button>
+          </Group>
+        </form>
+      </Stack>
     </Layout>
   );
 }
