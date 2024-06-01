4b2b54cd567b056da326e1325fb8992b5151a2f5
Alashybay <alikalikhan01@gmail.com>
2024-06-01 14:45:24 +0200

    profile front

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 
 import { Layout } from "@/src/components/Layout";
-import { Text } from "@mantine/core";
+import { Button, Group, NumberInput, Stack, Text, TextInput } from "@mantine/core";
 
 export default function Page() {
   return (
     <Layout>
-      <Text>Profile!</Text>
+      <Stack>
+        <Text size="xl">Profile</Text>
+        <TextInput label="Name" placeholder="John Doe" />
+        <TextInput label="Surname" placeholder="Doe" />
+        <TextInput label="Email" placeholder="user@gmail.com" />
+        <TextInput label="Password" type="password" placeholder="********" />
+        <NumberInput label="Age" placeholder="25" hideControls />
+        <NumberInput label="Phone" placeholder="1234567890" hideControls />
+        <Group justify="right">
+          <Button>Save</Button>
+        </Group>
+      </Stack>
     </Layout>
   );
 }
