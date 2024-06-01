030181f455faf466bf468f0297b28cb4bbe14e46
Alashybay <alikalikhan01@gmail.com>
2024-06-01 21:57:24 +0200

    added subscription page

--- src/app/menu/subscriptions/page.tsx
+++ src/app/menu/subscriptions/page.tsx
@@ -1,12 +1,80 @@
 "use client";
 
 import { Layout } from "@/src/components/Layout";
-import { Text } from "@mantine/core";
+import {
+  Button,
+  Divider,
+  Group,
+  NumberInput,
+  Paper,
+  Radio,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
 
 export default function Page() {
   return (
     <Layout>
-      <Text>Subscriptions!</Text>
+      <Stack>
+        <Title order={3} fw="normal">
+          Please fill out your form for subscription!
+        </Title>
+        <Text c="gray">
+          Choose a subscription plan to gain access to exclusive food recipes
+          and culinary content. Each plan offers unique benefits to enhance your
+          cooking experience.
+        </Text>
+        <Divider variant="dashed" />
+
+        <Radio.Group
+          label="Choose your plan:"
+          description="Subscription can be cancelled at any time by emailing our support."
+          withAsterisk
+        >
+          <Stack mt="md">
+            <Radio
+              value="gold"
+              label="Gold - $29.99/month (Includes all recipes, premium content, and monthly exclusive cooking webinars)"
+            />
+            <Radio
+              value="silver"
+              label="Silver - $19.99/month (Access to all recipes, excluding premium content)"
+            />
+            <Radio
+              value="bronze"
+              label="Bronze - $9.99/month (Access to basic recipes only)"
+            />
+          </Stack>
+        </Radio.Group>
+        <Divider variant="dashed" />
+        <Paper shadow="md" withBorder radius="md" p="lg">
+          <Stack>
+            <TextInput
+              label="Cardholder Name"
+              placeholder="Enter the name as it appears on your card"
+              required
+            />
+            <NumberInput
+              label="Card Number"
+              placeholder="Enter your card number"
+              hideControls
+              required
+            />
+            <TextInput label="Expiry Date" placeholder="MM/YY" required />
+            <NumberInput
+              label="CVV"
+              placeholder="Enter your CVV"
+              hideControls
+              required
+            />
+          </Stack>
+        </Paper>
+        <Group justify="right" mt="md">
+          <Button>Save and Subscribe</Button>
+        </Group>
+      </Stack>
     </Layout>
   );
 }
