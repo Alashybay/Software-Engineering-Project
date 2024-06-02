7d1e41c5e0bad3bd7dad810b256980d0a378427a
Alashybay <alikalikhan01@gmail.com>
2024-06-02 13:41:47 +0200

    added recomendation form survey modal

--- src/components/auth/register/index.tsx
+++ src/components/auth/register/index.tsx
@@ -7,14 +7,20 @@ import {
   Button,
   Divider,
   Anchor,
+  Stack,
+  Title,
+  MultiSelect,
+  NumberInput,
 } from "@mantine/core";
 import { RegisterForm } from "./registerForm";
 import { useCallback } from "react";
 import { useRouter } from "next/navigation";
+import { modals } from "@mantine/modals";
 
 export function Register(props: PaperProps) {
   const form = useForm();
   const router = useRouter();
+
   const handleSubmit = useCallback(() => {
     console.log(form.getDirty());
   }, [form]);
@@ -23,9 +29,52 @@ export function Register(props: PaperProps) {
     router.push("/signIn");
   }, [router]);
 
+  const handleModal = () => {
+    modals.open({
+      withCloseButton: false,
+      centered: true,
+      size: "lg",
+      children: (
+        <Stack>
+          <Title order={4} fw="normal" ta="center">
+            Help Us Personalize Your Experience
+          </Title>
+          <NumberInput
+            label="What's your age?"
+            description="Knowing your age helps us tailor our recommendations just for you!"
+            placeholder="Enter your age"
+            hideControls
+          />
+          <MultiSelect
+            label="Choose Your Favorite Food Categories"
+            description="Tell us what you love so we can recommend recipes you'll enjoy!"
+            placeholder="Select your favorite categories"
+            searchable
+            data={[
+              { value: "fast_food", label: "Fast Food" },
+              { value: "italian", label: "Italian" },
+              { value: "mexican", label: "Mexican" },
+              { value: "indian", label: "Indian" },
+              { value: "chinese", label: "Chinese" },
+              { value: "vegetarian", label: "Vegetarian" },
+              { value: "vegan", label: "Vegan" },
+              { value: "desserts", label: "Desserts" },
+              { value: "seafood", label: "Seafood" },
+              { value: "gluten_free", label: "Gluten Free" },
+              { value: "pizza", label: "Pizza" },
+            ]}
+          />
+          <Group justify="right">
+            <Button onClick={handleSubmit}>Save and Create Account</Button>
+          </Group>
+        </Stack>
+      ),
+    });
+  };
+
   return (
     <Paper radius="md" p="sm" withBorder {...props}>
-      <Text size="lg" fw={500}>
+      <Text size="lg" fw={500} ta="center">
         Welcome to Food Recipes
       </Text>
 
@@ -33,17 +82,17 @@ export function Register(props: PaperProps) {
 
       <form onSubmit={form.onSubmit(() => {})}>
         <RegisterForm />
-        <Group justify="space-between" mt="xl">
+        <Group justify="apart" mt="xl">
           <Anchor
             component="button"
             type="button"
-            c="dimmed"
+            color="dimmed"
             size="xs"
             onClick={handleSignUp}
           >
-            {"Already have an account? Login"}
+            Already have an account? Login
           </Anchor>
-          <Button type="submit" radius="xl" onClick={handleSubmit}>
+          <Button type="submit" radius="xl" onClick={handleModal}>
             Register
           </Button>
         </Group>
