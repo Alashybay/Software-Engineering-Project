326467c230a2515b619fe17413339df256a58535
Alashybay <alikalikhan01@gmail.com>
2024-06-03 19:26:58 +0200

    added create new user funciton in signUp

--- src/components/auth/register/index.tsx
+++ src/components/auth/register/index.tsx
@@ -11,31 +11,47 @@ import {
   Title,
   MultiSelect,
   NumberInput,
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Select,
 } from "@mantine/core";
-import { RegisterForm } from "./registerForm";
-import { useCallback } from "react";
+import { useCallback, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { modals } from "@mantine/modals";
 import { useCreateNewUser } from "@/src/hooks/createUser";
+import ReCAPTCHA from "react-google-recaptcha";
+import { User } from "@/src/typings/user";
 
 export function Register(props: PaperProps) {
-  const form = useForm();
   const router = useRouter();
   const createNewUser = useCreateNewUser();
+  const recaptchaRef = useRef<ReCAPTCHA>(null);
 
-  const handleSubmit = useCallback(() => {
-    console.log(form.getDirty());
-    handleModal();
-  }, [form]);
+  const form = useForm({
+    initialValues: {
+      firstname: "",
+      surname: "",
+      email: "",
+      password: "",
+      is_admin: 0,
+      preferences: "",
+      age: 0,
+    }
+  })
+
+  const handleRecaptcha = async () => {
+    const token = recaptchaRef.current?.getValue();
+    if (!token) {
+      alert("Please complete the recaptcha");
+    } else {
+      alert("Recaptcha completed successfully");
+    }
+  };
 
   const handleSignUp = useCallback(() => {
     router.push("/signIn");
   }, [router]);
-
-  // const handleSubmit = (event: React.FormEvent) => {
-  //   event.preventDefault();
-  //   createNewUser.mutate({ newUserInfo: { name, email } });
-  // };
 
   const handleModal = () => {
     modals.open({
@@ -52,8 +68,9 @@ export function Register(props: PaperProps) {
             description="Knowing your age helps us tailor our recommendations just for you!"
             placeholder="Enter your age"
             hideControls
+            {...form.getInputProps("age")}
           />
-          <MultiSelect
+          <Select
             label="Choose Your Favorite Food Categories"
             description="Tell us what you love so we can recommend recipes you'll enjoy!"
             placeholder="Select your favorite categories"
@@ -71,6 +88,7 @@ export function Register(props: PaperProps) {
               { value: "gluten_free", label: "Gluten Free" },
               { value: "pizza", label: "Pizza" },
             ]}
+            {...form.getInputProps("preferences")}
           />
           <Group justify="right">
             <Button onClick={handleSubmit}>Save and Create Account</Button>
@@ -79,6 +97,24 @@ export function Register(props: PaperProps) {
       ),
     });
   };
+
+
+  const handleSubmit = useCallback(() => {
+    const newUser: User = {
+      firstname: form.values.firstname,
+      surname: form.values.surname,
+      email: form.values.email,
+      password: form.values.password,
+      age: form.values.age,
+      preferences: form.values.preferences,
+      is_admin: 0,
+    };
+    console.log("new user", newUser);
+    console.log("form", form.values)
+
+    createNewUser.mutate({ newUserInfo: newUser });
+
+  }, [form, createNewUser]);
 
   return (
     <Paper radius="md" p="sm" withBorder {...props}>
@@ -89,18 +125,75 @@ export function Register(props: PaperProps) {
       <Divider label="Or continue with email" labelPosition="center" my="lg" />
 
       <form onSubmit={form.onSubmit(() => {})}>
-        <RegisterForm />
+        <Stack>
+          <TextInput
+            label="firstname"
+            placeholder="Your firstname"
+            value={form.values.firstname}
+            onChange={(event) =>
+              form.setFieldValue("firstname", event.currentTarget.value)
+            }
+            radius="md"
+          />
+          <TextInput
+            label="surname"
+            placeholder="Your surname"
+            value={form.values.surname}
+            onChange={(event) =>
+              form.setFieldValue("surname", event.currentTarget.value)
+            }
+            radius="md"
+          />
+
+          <TextInput
+            required
+            label="Email"
+            placeholder="your@email.com"
+            value={form.values.email}
+            key={form.key("email")}
+            {...form.getInputProps("email")}
+            error={form.errors.email && "Invalid email"}
+            radius="md"
+          />
+
+          <PasswordInput
+            required
+            label="Password"
+            placeholder="Your password"
+            value={form.values.password}
+            onChange={(event) =>
+              form.setFieldValue("password", event.currentTarget.value)
+            }
+            error={
+              form.errors.password &&
+              "Password should include at least 6 characters"
+            }
+            radius="md"
+          />
+
+          <Checkbox
+            label="I accept terms and conditions"
+            onChange={(event) =>
+              form.setFieldValue("terms", event.currentTarget.checked)
+            }
+          />
+          <ReCAPTCHA
+            ref={recaptchaRef}
+            sitekey={"6LecIu8pAAAAAA_PycEY_c4y8ZoWKVtyFkrmB6Z1"}
+            onChange={handleRecaptcha}
+          />
+        </Stack>
         <Group justify="apart" mt="xl">
           <Anchor
             component="button"
             type="button"
-            color="dimmed"
+            c="dimmed"
             size="xs"
             onClick={handleSignUp}
           >
             Already have an account? Login
           </Anchor>
-          <Button type="submit" radius="xl" onClick={handleSubmit}>
+          <Button type="submit" radius="xl" onClick={handleModal}>
             Register
           </Button>
         </Group>
