03d59fcaf5676bc984dc454da22860e26be9af02
Alashybay <alikalikhan01@gmail.com>
2024-06-03 09:47:29 +0200

    minor fix from ish

--- src/components/modals/role/index.tsx
+++ src/components/modals/role/index.tsx
@@ -24,6 +24,7 @@ export function UserRoleModal(props: {
           userId: props.user?.id,
           updatedUserInfo: {
             role,
+            is_admin:role==='admin' ? 1: 0
           },
         });
         props.onClose();
