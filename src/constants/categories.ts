39046a211c389ba2988ebafe80d7e0280f5f63bf
Ishen Imerov <macbook_pro@MacBook-Pro-MacBook-Pro.local>
2024-06-13 14:04:27 +0200

    sort, filter, search added

---
+++ src/constants/categories.ts
@@ -0,0 +1,20 @@
+export const categories = [
+    {
+        label: 'All', value: 'all'
+    },
+    {
+        label: 'Mexican', value: 'mexican'
+    },
+    {
+        label: 'Italian', value: 'italian'
+    },
+    {
+        label: 'Russian', value: 'russian'
+    },
+    {
+        label: 'Kyrgyz', value: 'kyrgyz'
+    },
+    {
+        label: 'Kazakh', value: 'kazakh'
+    },
+]
